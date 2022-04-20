c45b25c8ced59170cd1060c0cd81b8ffc00746e2
Adi3011 <aditya1207khare@gmail.com>
2022-04-20 17:28:28 +0530

    final

--- public/js/main.js
+++ public/js/main.js
@@ -42,48 +42,7 @@ const getInfo = async(event) =>{
                 "<i class = 'fas fa-sun' style='color: #eccc68;'></i>";
             }
 
-            const getCurrentDay = ( ) => {
-                let weekday = new Array(7);
-                weekday[0] = "Sunday";
-                weekday[1] = "Monday";
-                weekday[2] = "Tuesday";
-                weekday[3] = "Wednesday";
-                weekday[4] = "Thursday";
-                weekday[5] = "Friday";
-                weekday[6] = "Saturday";
-                let currentTime = new Date();
-                let day = weekday[currentTime.getDay()];
-
-                return day;
-            }
-
             
-
-            const getCurrentTime = ( ) =>{
-                var months = [
-                    'Jan',
-                    'Feb','Mar','April','May','June','July',
-                    'Aug','Sept','Oct','Nov','Dec'
-                ];
-
-                var now = new Date();
-                var hours = now.getHours();
-                var mins = now.getMinutes();
-                var month = now.getMonth();
-                var date = now.getDate();
-
-                var period = 'am';
-                if(hours > 11){
-                    period = 'pm';
-                    if(hours > 12 ) hours -= 12;
-                }
-                if(mins < 10){
-                    mins = '0' + mins;
-                }
-
-                return `${month} ${date} | ${hours}:${mins}${period}`;
-
-            }
             datahide.classList.remove('data_hide');
     
         }catch(err){
